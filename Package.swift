9dd0f4e2d77e2d0a533a8ad6d7a97fbdb05bbda3
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-06-13 21:36:59 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,12 +13,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LiveKitComponents",
-            targets: ["LiveKitComponents"]),
+            targets: ["LiveKitComponents"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "LiveKit", url: "https://github.com/livekit/client-sdk-swift.git", .exact("1.0.13")),
+        .package(name: "LiveKit", url: "https://github.com/livekit/client-sdk-swift.git", .exact("1.0.13"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,6 +31,6 @@ let package = Package(
         ),
         .testTarget(
             name: "LiveKitComponentsTests",
-            dependencies: ["LiveKitComponents"]),
+            dependencies: ["LiveKitComponents"])
     ]
 )
