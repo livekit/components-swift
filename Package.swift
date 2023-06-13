fc89046ac2acc2f71c030300b8ab857ff04c36cf
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-06-13 15:15:36 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "LiveKitComponents",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,13 +18,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "LiveKit", url: "https://github.com/livekit/client-sdk-swift.git", .exact("1.0.13")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LiveKitComponents",
-            dependencies: []),
+            dependencies: ["LiveKit"],
+            path: "Sources"
+        ),
         .testTarget(
             name: "LiveKitComponentsTests",
             dependencies: ["LiveKitComponents"]),
