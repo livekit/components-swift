5c07de917574ba5a601359c9ba55d7c098b3e44e
Jonas Schell <jonas@livekit.io>
2023-09-25 15:58:14 +0200

    add swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "LiveKit", url: "https://github.com/livekit/client-sdk-swift.git", .exact("1.0.13"))
+        .package(name: "LiveKit", url: "https://github.com/livekit/client-sdk-swift.git", .exact("1.0.13")),
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
