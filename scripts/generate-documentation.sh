b6473d432e920cca5fcf216ad63c51ad22254b84
Jonas Schell <jonas@livekit.io>
2023-10-16 16:04:48 +0200

    add doc gen script

---
+++ scripts/generate-documentation.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+mkdir -p .temp/docbuild-output
+mkdir -p .temp/doccarchives
+
+# Build the documentation.
+xcodebuild docbuild                  \
+  -scheme "LiveKitComponents"             \
+  -destination 'generic/platform=ios' \
+  -derivedDataPath .temp/docbuild-output
+  
+# Copy the documentation archive where we can find it.
+find .temp/docbuild-output               \
+  -name "*.doccarchive"              \
+  -exec cp -R {} .temp/doccarchives \;
+
+# Generate the static documentation site from the doccarchive.
+$(xcrun --find docc) process-archive \
+transform-for-static-hosting .temp/doccarchives/LiveKitComponents.doccarchive \
+--output-path docs \
+--hosting-base-path livekit.github.io/components-swift
