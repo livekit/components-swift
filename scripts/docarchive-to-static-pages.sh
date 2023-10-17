40bfefa4dc2078e7e8f73ae3a28b1326ed25b846
ocupe <jonas@livekit.io>
2023-10-17 12:30:53 +0200

    update docs

--- scripts/docarchive-to-static-pages.sh
+++ scripts/docarchive-to-static-pages.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 # Generate the static documentation site from the doccarchive.
-$(xcrun --find docc) process-archive transform-for-static-hosting .temp/doccarchives/LiveKitComponents.doccarchive --output-path docs --hosting-base-path components-swift
+$(xcrun --find docc) process-archive transform-for-static-hosting .temp/doccarchives/LiveKitComponents.doccarchive --output-path docs --hosting-base-path /components-swift
