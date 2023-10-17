16aa6eac5e33f19d48481927f83795a1927745f5
ocupe <jonas@livekit.io>
2023-10-17 14:28:39 +0200

    remove scripts folder

--- scripts/docarchive-to-static-pages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-# Generate the static documentation site from the doccarchive.
-$(xcrun --find docc) process-archive transform-for-static-hosting .temp/doccarchives/LiveKitComponents.doccarchive --output-path docs --hosting-base-path /components-swift
