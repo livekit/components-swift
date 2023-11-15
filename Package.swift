bb51e5408ee980aa1b1547e9db2fe88f50032e6c
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-11-16 06:42:13 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,21 +10,16 @@ let package = Package(
         .macOS(.v11),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LiveKitComponents",
             targets: ["LiveKitComponents"]
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "LiveKit", url: "https://github.com/livekit/client-sdk-swift.git", .exact("1.0.13")),
         .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.3.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LiveKitComponents",
             dependencies: ["LiveKit"],
