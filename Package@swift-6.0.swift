7e8245a692282879ad9aff74cac4f65490de9199
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-11-13 10:18:24 +0100

    Bump SDK

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/livekit/client-sdk-swift.git", from: "2.7.0"),
+        .package(url: "https://github.com/livekit/client-sdk-swift.git", from: "2.10.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.4.3"),
     ],
     targets: [
